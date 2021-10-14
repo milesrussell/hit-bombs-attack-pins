72c3de13ebb1224d62da26fff203f152af7d22c4
Miles Russell <milesstats@gmail.com>
2021-10-14 10:08:14 -0500

    adding fields to viz model

--- models/viz/shots_strokes_gained.sql
+++ models/viz/shots_strokes_gained.sql
@@ -10,6 +10,18 @@ shots_to_hole AS (
   FROM {{ ref('stg_shots_to_hole') }}
 ),
 
+rounds AS (
+  SELECT
+    *
+  FROM {{ ref('stg_rounds') }}
+),
+
+courses AS (
+  SELECT
+    *
+  FROM {{ ref('stg_courses') }}
+),
+
 shots_with_avgs AS (
   SELECT
     shots.shot_id,
@@ -19,22 +31,24 @@ shots_with_avgs AS (
     shots.distance,
     shots.round_id,
     shots_to_hole.pro_strokes_to_hole,
-    shots_to_hole.scratch_strokes_to_hole
+    shots_to_hole.scratch_strokes_to_hole,
+    rounds.day,
+    rounds.avg_temp AS round_avg_temp,
+    rounds.avg_windspeed_mph AS round_avg_windspeed_mph,
+    rounds.full_round,
+    courses.course_name,
+    courses.satellite_yardage AS course_satellite_yardage,
+    courses.scorecard_yardage AS course_scorecard_yardage
   FROM shots
   LEFT JOIN shots_to_hole ON shots.shot_type_id = shots_to_hole.shot_type
     AND shots.distance = shots_to_hole.distance
+  LEFT JOIN rounds ON rounds.round_id = shots.round_id
+  LEFT JOIN courses ON courses.course_id = rounds.course_id
 ),
 
 shots_with_results AS (
   SELECT
-    shot_id,
-    hole_shot_seq,
-    shot_type,
-    hole_id,
-    distance,
-    round_id,
-    pro_strokes_to_hole,
-    scratch_strokes_to_hole,
+    *,
 
     {% set fields = ['distance', 'shot_type', 'pro_strokes_to_hole', 'scratch_strokes_to_hole'] %}
 
@@ -73,8 +87,20 @@ SELECT
     WHEN shot_type = 'Penalty' THEN 0 -- strokes lost due to penalty are applied to the shot preceding the penalty stroke.
     WHEN resulting_shot_type = 'Penalty' THEN {{ toggle }}_strokes_to_hole - two_strokes_later_{{ toggle }}_strokes_to_hole - 2 -- if a stroke results in a penalty, use the average strokes to hole from the shot following the penalty.
     ELSE {{ toggle }}_strokes_to_hole - resulting_{{ toggle }}_strokes_to_hole - 1
-  END AS strokes_gained_{{ toggle }}
+  END AS strokes_gained_{{ toggle }},
 
-  {% if not loop.last %} , {% endif %}
   {% endfor %}
+
+  COUNT(DISTINCT hole_id) OVER(
+    PARTITION BY
+      round_id
+  ) AS holes_in_round,
+  COUNT(shot_id) OVER(
+    PARTITION BY
+      round_id
+  ) AS shots_in_round,
+  SUM(CASE WHEN hole_shot_seq = 1 THEN distance ELSE 0 END) OVER(
+    PARTITION BY
+      round_id
+  ) AS round_yardage
 FROM shots_with_results
